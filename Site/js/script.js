b167265491f9485828769a12390689b10730056d
Sandip Swain <swainsandip9@gmail.com>
2021-04-06 07:28:25 +0530

    Added responsiveness to certified icons and changed the btn color

--- Site/js/script.js
+++ Site/js/script.js
@@ -1,9 +1,35 @@
 const cards = document.querySelectorAll(".cards");
-
+const buttons = document.querySelectorAll(".btn");
 const cardImg = document.querySelectorAll(".cards-img");
 const cardTitle = document.querySelectorAll(".cards-title");
 const cardDesc = document.querySelectorAll(".cards-desc");
 const cardBtn = document.querySelectorAll(".cards-btn");
+
+// Button Ripple Effect
+buttons.forEach((button) => {
+  button.addEventListener("mouseover", function (e) {
+    //   Position from the viewport from the top
+    const x = e.clientX;
+    // Position from the viewport from the left
+    const y = e.clientY;
+
+    const btnTop = e.target.offsetTop;
+    const btnLeft = e.target.offsetLeft;
+
+    const xInside = x - btnLeft;
+    const yInside = y - btnTop;
+
+    const circle = document.createElement("span");
+    circle.classList.add("circle");
+    circle.style.top = yInside + "px";
+    circle.style.top = xInside + "px";
+
+    this.appendChild(circle);
+
+    setTimeout(() => circle.remove(), 500);
+  });
+});
+// Button Ripple Effect ends
 
 for (let i = 0; i <= 5; i++) {
   // Animate mouse in
